4711b195637768f081482c299c94e976767fc1c4
Rachma Alifia <rachmalifia@gmail.com>
2023-05-01 20:32:29 +0700

    fixing layout page two

--- app/src/main/java/com/fikri/ecoguardian/TwoActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/TwoActivity.kt
@@ -1,11 +1,29 @@
 package com.fikri.ecoguardian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fikri.ecoguardian.databinding.ActivityMainBinding
+import com.fikri.ecoguardian.databinding.ActivityTwoBinding
 
 class TwoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTwoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_two)
+        binding = ActivityTwoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPrev.setOnClickListener {
+            Intent(this@TwoActivity, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        binding.btnNext.setOnClickListener {
+            Intent(this@TwoActivity, ThreeActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
 }
