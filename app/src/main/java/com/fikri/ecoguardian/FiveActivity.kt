d30e529e6abaff4017bd3c786f28c6a770d4f87c
Rachma Alifia <rachmalifia@gmail.com>
2023-05-02 01:42:02 +0700

    fixing layout page four and five

--- app/src/main/java/com/fikri/ecoguardian/FiveActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/FiveActivity.kt
@@ -1,11 +1,30 @@
 package com.fikri.ecoguardian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fikri.ecoguardian.databinding.ActivityFiveBinding
+import com.fikri.ecoguardian.databinding.ActivityTwoBinding
 
 class FiveActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFiveBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_five)
+        binding = ActivityFiveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnPrev.setOnClickListener {
+                Intent(this@FiveActivity, FourActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnNext.setOnClickListener {
+                Intent(this@FiveActivity, SixActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/fikri/ecoguardian/FourActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/FourActivity.kt
@@ -1,11 +1,31 @@
 package com.fikri.ecoguardian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fikri.ecoguardian.databinding.ActivityFourBinding
+import com.fikri.ecoguardian.databinding.ActivityTwoBinding
 
 class FourActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_four)
+        binding = ActivityFourBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnPrev.setOnClickListener {
+                Intent(this@FourActivity, ThreeActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnNext.setOnClickListener {
+                Intent(this@FourActivity, FiveActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
