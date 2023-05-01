79dfa3abada4f3824d5cab652b8d21e06a51c6f9
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2023-05-01 19:58:20 +0700

    Add Layout TwentyFive

---
+++ app/src/main/java/com/fikri/ecoguardian/TwentyFiveActivity.kt
@@ -0,0 +1,11 @@
+package com.fikri.ecoguardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwentyFiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twenty_five)
+    }
+}
