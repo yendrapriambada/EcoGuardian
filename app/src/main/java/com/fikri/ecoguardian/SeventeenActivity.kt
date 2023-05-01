1ce5b97a20e74b8ad432caa7e761fa78300e1eab
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2023-05-01 14:40:25 +0700

    Add Layout Seventeen

---
+++ app/src/main/java/com/fikri/ecoguardian/SeventeenActivity.kt
@@ -0,0 +1,11 @@
+package com.fikri.ecoguardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeventeenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seventeen)
+    }
+}
