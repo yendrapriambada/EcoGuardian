8fa903503fc89608e79c787304e777a1a0e0df34
Mochamad Yendra Priambada <yendrapriambada1234@gmail.com>
2023-05-01 15:03:43 +0700

    Add Layout Nineteen

---
+++ app/src/main/java/com/fikri/ecoguardian/NineteenActivity.kt
@@ -0,0 +1,11 @@
+package com.fikri.ecoguardian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NineteenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nineteen)
+    }
+}
