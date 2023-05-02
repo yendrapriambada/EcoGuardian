d36b40814e31d43209c8a193cf1b775ee6521eaf
Rachma Alifia <rachmalifia@gmail.com>
2023-05-02 12:49:34 +0700

    add layout page thirteen

--- app/src/main/java/com/fikri/ecoguardian/ThirteenActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/ThirteenActivity.kt
@@ -1,11 +1,31 @@
 package com.fikri.ecoguardian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fikri.ecoguardian.databinding.ActivityElevenBinding
+import com.fikri.ecoguardian.databinding.ActivityThirteenBinding
 
 class ThirteenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirteenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_thirteen)
+        binding = ActivityThirteenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnPrev.setOnClickListener {
+                Intent(this@ThirteenActivity, ElevenActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnNext.setOnClickListener {
+                Intent(this@ThirteenActivity, FourteenActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
