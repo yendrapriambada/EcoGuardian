680d6432136018565582e4d3adf5ce1d614db969
Rachma Alifia <rachmalifia@gmail.com>
2023-05-02 12:25:52 +0700

    add layout page eleven

--- app/src/main/java/com/fikri/ecoguardian/ElevenActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/ElevenActivity.kt
@@ -4,24 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fikri.ecoguardian.databinding.ActivityEightBinding
-import com.fikri.ecoguardian.databinding.ActivityNineBinding
+import com.fikri.ecoguardian.databinding.ActivityElevenBinding
 
-class NineActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityNineBinding
+class ElevenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityElevenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNineBinding.inflate(layoutInflater)
+        binding = ActivityElevenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.apply {
             btnPrev.setOnClickListener {
-                Intent(this@NineActivity, EightActivity::class.java).also {
+                Intent(this@ElevenActivity, TenActivity::class.java).also {
                     startActivity(it)
                 }
             }
 
             btnNext.setOnClickListener {
-                Intent(this@NineActivity, TenActivity::class.java).also {
+                Intent(this@ElevenActivity, ThirteenActivity::class.java).also {
                     startActivity(it)
                 }
             }
--- app/src/main/java/com/fikri/ecoguardian/TenActivity.kt
+++ app/src/main/java/com/fikri/ecoguardian/TenActivity.kt
@@ -1,11 +1,30 @@
 package com.fikri.ecoguardian
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fikri.ecoguardian.databinding.ActivityEightBinding
+import com.fikri.ecoguardian.databinding.ActivityTenBinding
 
 class TenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ten)
+        binding = ActivityTenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnPrev.setOnClickListener {
+                Intent(this@TenActivity, NineActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+
+            btnNext.setOnClickListener {
+                Intent(this@TenActivity, NineActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 }
